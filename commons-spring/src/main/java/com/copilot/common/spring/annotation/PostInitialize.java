2e8e1fe848fa95470d091a73c3bec965ea1b5a3e
Rico Yu <ricoyu520@gmail.com>
2025-03-05 17:30:35 +0800

    添加commons-spring模块

---
+++ commons-spring/src/main/java/com/copilot/common/spring/annotation/PostInitialize.java
@@ -0,0 +1,53 @@
+package com.copilot.common.spring.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+/**
+ * Annotation that marks a method to be executed after initialize complete.<br/>
+ * Initialize complete means all properties of this bean is set and transaction is ready.
+ * 
+ * 注意@PostInitialize标注的方法执行的时候不要有阻塞行为, 不然如果有多个@PostInitialize方法, 后面的可能就一直得不到执行
+ * @author xuehuyu
+ * @since Mar 19, 2015
+ * @version 1.0
+ *
+ */
+@Inherited
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PostInitialize {
+    /**
+     * Defines the order used to initialize the method
+     *
+     * @return The order
+     * @since 1.0
+     */
+    int order() default 0;
+    
+    /**
+     * 分组执行，每组内的按照order依次执行
+     * @return
+     */
+    String group() default "";
+    
+    /**
+     * 默认立即执行
+     * @return
+     */
+    int delay() default 0;
+    
+    /**
+     * 延迟执行的单位，默认分钟
+     * @return
+     */
+    TimeUnit delayUnit() default MINUTES;
+}
---
+++ commons-spring/src/main/java/com/copilot/common/spring/aspect/SmartLoggerAspect.java
@@ -0,0 +1,59 @@
+package com.copilot.common.spring.aspect;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * Copyright: (C), 2020/4/11 17:43
+ * <p>
+ * <p>
+ * Company: Sexy Uncle Inc.
+ *
+ * @author Rico Yu ricoyu520@gmail.com
+ * @version 1.0
+ */
+@Aspect
+@Slf4j
+@Component
+public class SmartLoggerAspect {
+
+	private static final Logger logger = LoggerFactory.getLogger(SmartLoggerAspect.class);
+
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	{
+		objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		//JSON字符串field允许使用单引号括起来, 标准JSON是不允许的, 只可以使用双引号
+		objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+		//对POJO字段排序
+		objectMapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+	}
+	
+	@Pointcut("@annotation(com.copilot.common.spring.annotation.SmartLogger)")
+	public void pointcut() {
+	}
+	
+	@Before("pointcut()")
+	public void before(JoinPoint joinPoint) {
+		log.info("方法名: {}", joinPoint.getSignature().getName());
+		try {
+			log.info("参数: {}", objectMapper.writeValueAsString(joinPoint.getArgs()));
+		} catch (JsonProcessingException e) {
+			logger.error("序列化方法参数失败: ", e);
+		}
+	}
+	
+}
