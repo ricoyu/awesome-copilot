b71dc9a8fbf814c5dadb4077e5739d0707af73b2
Rico Yu <ricoyu520@gmail.com>
2025-07-28 10:28:15 +0800

    刷题

--- copilot-test/src/test/java/com/awesomecopilot/algorithm/leetcode/MinCostClimbingStairs2.java
+++ copilot-test/src/test/java/com/awesomecopilot/algorithm/leetcode/MinCostClimbingStairs2.java
@@ -1,4 +1,74 @@
 package com.awesomecopilot.algorithm.leetcode;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 746. 使用最小花费爬楼梯
+ * <p>
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ * <p>
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ * <p>
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ *
+ * <pre>
+ * 示例 1：
+ *
+ * 输入：cost = [10,15,20]
+ * 输出：15
+ * 解释：你将从下标为 1 的台阶开始。
+ * - 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+ * 总花费为 15 。
+ * </pre>
+ *
+ * <pre>
+ * 示例 2：
+ *
+ * 输入：cost = [1,100,1,1,1,100,1,1,100,1]
+ * 输出：6
+ * 解释：你将从下标为 0 的台阶开始。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 2 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 4 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 6 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达下标为 7 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 9 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达楼梯顶部。
+ * 总花费为 6 。
+ * </pre>
+ * <p/>
+ * Copyright: Copyright (c) 2025-07-28 7:51
+ * <p/>
+ * Company: Sexy Uncle Inc.
+ * <p/>
+
+ * @author Rico Yu  ricoyu520@gmail.com
+ * @version 1.0
+ */
 public class MinCostClimbingStairs2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("请输入cost数组: ");
+		int[] cost = Arrays.stream(scanner.nextLine().trim().split(",")).mapToInt(Integer::parseInt).toArray();
+		System.out.println(minCostClimbingStairs(cost));
+	}
+
+	public static int minCostClimbingStairs(int[] cost) {
+		int n = cost.length;
+
+		// dp[i] 表示到达第 i 个台阶顶部的最低花费
+		int[] dp = new int[n + 1];
+
+		// 初始化前两个台阶，开始可以从第0或第1个台阶起跳，不需要花费
+		dp[0] = 0;
+		dp[1] = 0;
+
+		// 动态规划，从第2个台阶开始计算
+		for (int i = 2; i <= n; i++) {
+		    dp[i] = Math.min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+		}
+
+		return dp[n];
+	}
 }
