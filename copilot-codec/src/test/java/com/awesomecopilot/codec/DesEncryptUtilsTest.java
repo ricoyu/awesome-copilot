618f3d07eac9ed216d5056d6021de015bc591abd
RicoYu <ricoyu520@gmail.com>
2026-07-19 17:35:07 +0800

    chore(test): 更新测试配置和添加订单数据
    - 修改DES加密工具测试中的邮箱地址
    - 更新Elasticsearch测试配置的主机地址
    - 添加大量电商订单测试数据用于批量操作测试

--- copilot-codec/src/test/java/com/awesomecopilot/codec/DesEncryptUtilsTest.java
+++ copilot-codec/src/test/java/com/awesomecopilot/codec/DesEncryptUtilsTest.java
@@ -8,7 +8,7 @@ public class DesEncryptUtilsTest {
 	
 	@Test
 	public void testEncryptJaJaPwd() {
-		String encrypt = DesEncryptUtils.encrypt("123456", "ricoyu_521@hotmail.com");
+		String encrypt = DesEncryptUtils.encrypt("123456", "ricoyu@jajalink.com");
 		System.out.println(encrypt);
 	}
 	
